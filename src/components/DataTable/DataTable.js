f32de8bdec7ff83ccd223816c88990c4f979caba
ddenis87 <dizdok@yandex.ru>
2021-03-18 18:26:48 +0900

    Scroll...

--- src/components/DataTable/DataTable.js
+++ src/components/DataTable/DataTable.js
@@ -41,12 +41,14 @@ export const DataTable = {
       if (this.listDataCount == 0) {
         this.isDialogEmptyShow = true;
       }
-      // if (this.listDataCount < 50) {
+      if (this.listDataCount < 50) {
       //   if (this.getApiNext())
       //     this.requestData({next: true});
-      //   if (this.getApiPrevious())
-      //     this.requestData({previous: true});
-      // }
+        if (this.getApiPrevious()) {
+          console.log('previous load');
+          // this.requestData({previous: true});
+        }
+      }
     },
   },
   async created() {
