eae30dde13df605411ac3c7dbd73a5efde49a047
ddenis87 <dizdok@yandex.ru>
2021-04-01 04:47:53 +0900

    Create new component

---
+++ src/components/TableForm/TableForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="table-form">
+    <table class="table">
+      <thead class="table-head"></thead>
+      <tbody class="table-body"></tbody>
+      <tfoot class="table-foot"></tfoot>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TableForm',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
