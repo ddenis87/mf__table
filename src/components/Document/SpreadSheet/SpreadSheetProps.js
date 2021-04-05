cfa9621c7df6653cec8133e32d89e308ff207e2b
ddenis87 <dizdok@yandex.ru>
2021-04-05 15:26:24 +0900

    Props spreadsheet

--- src/components/Document/SpreadSheet/SpreadSheetProps.js
+++ src/components/Document/SpreadSheet/SpreadSheetProps.js
@@ -1,30 +1,39 @@
-export const SpreadSheetProps = {
+export default {
   props: {
-    columns: { // столбцы
-      type: Array,
+    spreadSheet: {
+      type: Object,
       default() {
         return {
-          name: String,
-          style: { type: [String, Array], default: '' },
-        };
-      },
-    },
-    columnsStyles: { // стили столбцов
-      type: Array,
-      default() {
-        return {
-          name: String,
-          value: {
+          style: {
             type: Object,
             default() {
               return {
-                minWidth: String,
+                fontFamily: { type: String, default: 'Arial, Helvetica, sans-serif' },
+                fontSize: { type: String, default: '16px' },
               };
             },
           },
         };
       },
     },
+
+    columns: {
+      type: Array,
+      default() {
+        return {
+          name: String,
+          style: {
+            type: Object,
+            default() {
+              return {
+                maxWidth: String,
+              };
+            },
+          },
+        };
+      },
+    },
+
     cells: { // ячейки
       type: Array,
       default() {
@@ -32,7 +41,7 @@ export const SpreadSheetProps = {
           name: String,
           spanColRow: { type: [Number, Array], default: 1 },
           value: { type: [String, Number, Boolean, Array, Object, Date, Function], default: '' },
-          style: { type: [String, Array], default: '' },
+          class: { type: String, default: '' },
           protected: { type: Boolean, default: false },
         };
       },
@@ -41,10 +50,32 @@ export const SpreadSheetProps = {
       type: Array,
       default() {
         return {
-          name: { type: String, default: '' },
-          value: { type: Object, default() { return { }; } },
+          name: String,
+          list: {
+            type: Object,
+            default() {
+              return {
+                fontFamily: { type: String, default: '' },
+                fontSize: { type: String, default: '' },
+                fontWeight: { type: String, default: '' }, //  normal, lighter, bold, bolder
+                fontStyle: { type: String, default: '' }, //  normal, italic, oblique
+                textDecoration: { type: String, default: '' }, // underline, overline, line-through
+                textTransform: { type: String, default: '' },
+                color: { type: String, default: '' },
+
+                textAlign: { type: String, default: '' },
+                verticalAlign: { type: String, default: '' },
+
+                borderTop: { type: String, default: '' },
+                borderRight: { type: String, default: '' },
+                borderBottom: { type: String, default: '' },
+                borderLeft: { type: String, default: '' },
+                backgroundColor: { type: String, default: '' },
+              };
+            },
+          },
         };
       },
     },
   },
-}
+};
